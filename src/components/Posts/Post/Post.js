7967c9fa013a066cecc37059cbb9988083a8ed0c
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-06-20 15:04:52 +0600

    so far so good

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import useStyles from './styles'
+
+const Post = () => {
+    const classes = useStyles()
+    return (
+        <div>
+            <h2>Single Post</h2>
+        </div>
+    );
+};
+
+export default Post;
